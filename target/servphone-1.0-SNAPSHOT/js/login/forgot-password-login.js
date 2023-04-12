b5b4a9431c6a766800818dd87d3b471f988f8c13
dariokrugerjunior <krugerdario1@gmail.com>
2023-04-12 20:41:04 -0300

    Generate .war

--- target/servphone-1.0-SNAPSHOT/js/login/forgot-password-login.js
+++
@@ -1,7 +0,0 @@
-function verificationUser() {
-    var email = document.getElementById('inputEmail').value
-    var phone = document.getElementById('inputPhone').value
-
-    if (!email || !phone) {
-    }
-}
